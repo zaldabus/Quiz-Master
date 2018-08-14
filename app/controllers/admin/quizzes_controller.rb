5e8429dc89169f4b38dc07a3e824d0c04458366b
Ryan Beckman <beckman.ryan@gmail.com>
2018-08-14 23:02:09 +0900

    Add boilerplate logic to Admin::QuizzesController

--- app/controllers/admin/quizzes_controller.rb
+++ app/controllers/admin/quizzes_controller.rb
@@ -1,25 +1,39 @@
 class Admin::QuizzesController < AdminController
 	def index
-
+		@quizzes = Quiz.all
 	end
 
 	def new
-
+		@quiz = Quiz.new
 	end
 
 	def create
+		@quiz = Quiz.new(quiz_params)
 
+		if @quiz.save
+			redirect_to admin_quizzes_path
+		else
+			render :new
+		end
 	end
 
 	def edit
-
+		@quiz = Quiz.find params[:id]
 	end
 
 	def update
+		@quiz = Quiz.find params[:id]
 
+		if @quiz.update_attributes(quiz_params)
+			redirect_to admin_quizzes_path
+		else
+			render :edit
+		end
 	end
 
 	def destroy
-
+		@quiz = Quiz.find params[:id]
+		@quiz.destroy
+		redirect_to admin_quizzes_path
 	end
 end
