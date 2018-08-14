11b7eac66d71cdb220ffd55a71e7435c6248833c
Ryan Beckman <beckman.ryan@gmail.com>
2018-08-14 17:44:08 +0900

    Add basic routes for different sections

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
+	namespace :admin do
+		resources :quizzes
+	end
+
+	namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: '/' do
+		namespace :v1 do
+			controller :quizzes do
+				get 'quizzes/request_quizzes', action: :request_quizzes
+			end
+		end
+	end
+
+	namespace :quiz_mode, path: '/' do
+		root to: :index, action: :index
+	end
+
   mount MagicLamp::Genie, at: "/magic_lamp" if defined?(MagicLamp)
 end
