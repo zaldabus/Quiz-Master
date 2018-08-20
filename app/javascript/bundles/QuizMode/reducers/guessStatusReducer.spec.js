b171aa39449e08c52dd4be1d07fc572c9f838af6
Ryan Beckman <beckman.ryan@gmail.com>
2018-08-20 10:33:50 +0900

    Add tests for guessStatusReducer

---
+++ app/javascript/bundles/QuizMode/reducers/guessStatusReducer.spec.js
@@ -0,0 +1,30 @@
+import expect from 'expect';
+
+import * as actions from '../actions/quizModeActionCreators';
+import guessStatus from './guessStatusReducer';
+
+describe('Guess Status Reducer', () => {
+ it('should change the state value of guessStatus to correct when passed QUIZ_MODE_ANSWER_CORRECT', () => {
+ 		const initialState = '';
+ 		const action = actions.setCorrect();
+ 		const newState = guessStatus(initialState, action);
+
+ 		expect(newState).toEqual('correct');
+ });
+
+ it('should change the state value of guessStatus to incorrect when passed QUIZ_MODE_ANSWER_INCORRECT', () => {
+ 		const initialState = '';
+ 		const action = actions.setIncorrect();
+ 		const newState = guessStatus(initialState, action);
+
+ 		expect(newState).toEqual('incorrect');
+ });
+
+ it('should reset the state value of guessStatus when passed QUIZ_MODE_RESET', () => {
+ 		const initialState = '';
+ 		const action = actions.tryAgain();
+ 		const newState = guessStatus(initialState, action);
+
+ 		expect(newState).toEqual('');
+ });
+});
